003a90efc4fc332571b9a58b5ab6bd470493c655
Anna Sibova <anna_sibova@ukr.net>
2023-02-21 22:58:30 +0200

    add 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem(CURRENT_TIME_KEY, seconds);
+}
+setCurrentTime();
+function setCurrentTime() {
+  if (!localStorage.getItem(CURRENT_TIME_KEY)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import { throttle } from 'lodash';
+const LOCAL_FORM_KEY = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+// console.log(formEl);
 
+feedbackForm();
+
+formEl.addEventListener('submit', onFormSubmit);
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const email = e.currentTarget.elements.email.value;
+  const message = e.currentTarget.elements.message.value;
+  const formData = {
+    email,
+    message,
+  };
+  email === '' || message === ''
+    ? alert('Відсутні дані')
+    : console.log(formData),
+    e.currentTarget.reset();
+
+  localStorage.removeItem(LOCAL_FORM_KEY);
+}
+
+function onFormInput(e) {
+  let savedValues = localStorage.getItem(LOCAL_FORM_KEY);
+  savedValues = savedValues ? JSON.parse(savedValues) : {};
+  savedValues[e.target.name] = e.target.value;
+  localStorage.setItem(LOCAL_FORM_KEY, JSON.stringify(savedValues));
+}
+
+function feedbackForm() {
+  let savedValue = localStorage.getItem(LOCAL_FORM_KEY);
+  if (savedValue) {
+    savedValue = JSON.parse(savedValue);
+    Object.entries(savedValue).forEach(([name, value]) => {
+      formEl.elements[name].value = value;
+    });
+  }
+}
